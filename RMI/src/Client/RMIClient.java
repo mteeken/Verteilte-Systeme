62a3d7cd77897b11b14a0e056444cb7a77bf1aaa
PNardman <philipp.nardmann@hs-osnabrueck.de>
2014-02-03 21:54:28 +0100

    Client angefangen, Interface geschrieben, Verbindungsscheme übertragen Signed-off-by:PNardman <philipp.nardmann@hs-osnabrueck.de>

--- RMI/src/Client/RMIClient.java
+++ RMI/src/Client/RMIClient.java
@@ -4,10 +4,36 @@
  */
 package Client;
 
+import Interface.Constant;
+import Interface.RemoteInterface;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /**
  *
- * @author Nugget
+ * @author Philipp Nardmann
  */
 public class RMIClient {
+    private Registry registry;
+    private RemoteInterface remote;
     
+    public RMIClient(){
+        
+    }
+    
+    public static void main(String args[]){
+      RMIClient rmi =  new RMIClient();
+      rmi.verbindungHerstellen();
+    }
+    
+    private void verbindungHerstellen(){
+        try{
+            this.registry = LocateRegistry.getRegistry("localhost", Constant.RMI_PORT);
+            this.remote = (RemoteInterface) registry.lookup(Constant.RMI_ID);
+        }catch(RemoteException | NotBoundException e){
+            System.out.println("Verbindung fehlgeschlagen");
+        }
+    }
 }
---
+++ RMI/src/Interface/Constant.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Nugget
+ */
+public class Constant {
+    public static final String RMI_ID = "RMI";
+    public static final int RMI_PORT = 2222;
+    
+}
