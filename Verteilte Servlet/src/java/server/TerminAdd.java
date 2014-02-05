562feb6663bb29dc65761f1bdcbc0976b864e78a
mteeken <mteeken@localhost>
2014-02-05 21:21:31 +0100

    Servlet angepasst

--- Verteilte Servlet/src/java/server/TerminAdd.java
+++ Verteilte Servlet/src/java/server/TerminAdd.java
@@ -6,8 +6,11 @@
 
 package server;
 
+import controller.NutzerController;
 import controller.TermineController;
+import exceptions.PasswordEmptyException;
 import java.io.IOException;
+import java.security.DigestException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -16,6 +19,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import module.Nutzer;
 import module.Terminart;
 import module.Termine;
 
@@ -30,10 +35,37 @@ public class TerminAdd extends HttpServlet {
      private Boolean sendRedirect = false;
     
          
-    protected void processRequest(HttpServletRequest req, HttpServletResponse res)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         
+        HttpSession session = request.getSession();
+        
+        if (request.getMethod().equals("POST")) {
+            String title = request.getParameterValues("title")[0];
+            String date_begin = request.getParameterValues("date_begin")[0];
+            String date_end = request.getParameterValues("date_end")[0];
+            String ort = request.getParameterValues("ort")[0];
+            Terminart art = Terminart.valueOf(request.getParameterValues("art")[0]);
+
+            TermineController tc = new TermineController();
+            
+            try {
+                tc.setTermin(title, date_begin, date_end, ort, art, session.getAttribute("name").toString());
+            } catch (PasswordEmptyException e) {
+                this.errorMessage = e.getMessage();
+            } catch (DigestException e) {
+                this.errorMessage = e.getMessage();
+            } catch (Exception e) {
+                this.errorMessage = e.getMessage();
+            }
+
+
+            this.sendRedirect = true;
+            response.sendRedirect("terminlist.jsp");
+        }
     }
+    
+    
      // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -45,7 +77,12 @@ public class TerminAdd extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-       
+        HttpSession session = null;
+        session = request.getSession();
+        if (session == null) {
+            response.sendRedirect("index.jsp");
+        } 
+
        response.setContentType("text/html;charset=UTF-8");
         // Ausgabe setzen
        ServletOutputStream writer; 
@@ -67,13 +104,13 @@ public class TerminAdd extends HttpServlet {
        try {
             writer.println("<form action=\"addtermin.jsp\" method=\"POST\">");
                 writer.println("<label for=\"title\">Title:</label>");
-                writer.println("<input type=\"text\" id=\"title\" name=\"title\" />");
+                writer.println("<input type=\"text\" id=\"title\" name=\"title\" /><br>");
                 writer.println("<label for=\"datum_begin\">Anfang:</label>");
-                writer.println("<input type=\"date\" name=\"date_begin\" />");
+                writer.println("<input type=\"date\" name=\"date_begin\" /><br>");
                 writer.println("<label for=\"datum_begin\">Ende:</label>");
-                writer.println("<input type=\"date\" name=\"date_end\" />");
+                writer.println("<input type=\"date\" name=\"date_end\" /><br>");
                 writer.println("<label for=\"datum_begin\">Ort:</label>");
-                writer.println("<input type=\"text\" name=\"ort\" />");
+                writer.println("<input type=\"text\" name=\"ort\" /><br>");
                 writer.println("<label for=\"datum_begin\">Art:</label>");
                 writer.println("<select name=\"art\">");
                     writer.println("<option value=\""+ Terminart.BIRTHDAY +"\">Geburtstag</option>");    
@@ -104,9 +141,16 @@ public class TerminAdd extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        HttpSession session = null;
+        session = request.getSession();
+        if (session == null) {
+            response.sendRedirect("index.jsp");
+        } else {
+            processRequest(request, response);
         
-        if (this.sendRedirect == false)
-            this.doGet(request, response);
+            if (this.sendRedirect == false)
+                this.doGet(request, response);
+        }
+
     }
 }
